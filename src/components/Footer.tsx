7558c2386bbaa81584d32d02f471ea89d725f0ab
Khaled Ashraf <khaledashrafelmaleh@gmail.com>
2024-11-26 02:33:54 +0200

    modified resume

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
             <div className="container mx-auto pt-4 text-white h-full">
                 <div className="flex justify-center items-center h-[90%] gap-[10rem]">
                     <h2 className="hover:underline"><a href="mailto:khaledaelmaleh@gmail.com">khaledaelmaleh@gmail.com</a></h2>
-                    <h2 className="hover:underline"><a href="/Khaled Ashraf resume new.pdf" target="_blank" rel="noopener noreferrer">Resume</a></h2>
+                    <h2 className="hover:underline"><a href="/Khaled Ashraf resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></h2>
                 </div>
                 <p className="w-full text-center text-base text-white/80">&copy; {new Date().getFullYear()} Khaled Ashraf. All rights reserved.</p>
             </div>
