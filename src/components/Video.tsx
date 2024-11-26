78a151042551ff6b3fe4f965e76b260e49772a62
Khaled Ashraf <khaledashrafelmaleh@gmail.com>
2024-11-26 21:13:05 +0200

    modifying video component to disable any input from user

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 interface VideoProps {
     src: string;
@@ -9,8 +9,27 @@ interface VideoProps {
 }
 
 const Video: React.FC<VideoProps> = ({ src, className, style }) => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        const videoElement = videoRef.current;
+        if (!videoElement) return;
+
+        // Ensure video starts playing immediately
+        const playVideo = async () => {
+            try {
+                await videoElement.play();
+            } catch (err) {
+                console.error('Autoplay was prevented:', err);
+            }
+        };
+
+        playVideo();
+    }, [src]);
+
     return (
         <video
+            ref={videoRef}
             src={src}
             className={`${className} object-cover`}
             style={style}
@@ -19,9 +38,12 @@ const Video: React.FC<VideoProps> = ({ src, className, style }) => {
             loop
             playsInline
             disablePictureInPicture
-            controls={false}
-            controlsList="nodownload noplaybackrate noremoteplayback"
             webkit-playsinline="true"
+            x-webkit-airplay="deny"
+            x5-video-player-type="none"
+            x5-video-player-fullscreen="false"
+            x5-video-orientation="portraint"
+            controlsList="nodownload noremoteplayback"
         />
     );
 };
