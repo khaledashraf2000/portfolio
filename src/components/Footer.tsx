63128aa1bc2d489fdcad5c25b8f2b7d8eba34b0a
Khaled Ashraf <khaledashrafelmaleh@gmail.com>
2024-12-22 23:38:14 +0200

    adjusted footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 export default function Footer() {
     return (
-        <footer className="bg-slate-900 h-96 mt-36 text-white">
-            <div className="flex flex-col justify-between h-full pt-16 pb-8 px-16">
-                <div className="flex justify-between items-top gap-4">
-                    <h4 className="logo">Khaled Ashraf</h4>
-                    <div className="flex gap-16">
+        <footer className="bg-slate-900 mt-36 text-white">
+            <div className="container mx-auto flex flex-col justify-between h-full pt-16 pb-8 px-16">
+                <div className="flex flex-col md:flex-row justify-between items-top gap-4">
+                    <div className="w-full flex justify-center md:justify-start mb-12">
+                        <h4 className="logo">Khaled Ashraf</h4>
+                    </div>
+                    <div className="flex justify-between md:justify-normal mb-12 md:gap-12">
                         <div className="flex flex-col gap-4">
                             <h4 className="uppercase text-white/80 text-sm">Socials</h4>
                             <p><a href="https://www.instagram.com/designedbykhaled" target="_blank" rel="noopener noreferrer">Instagram</a></p>
@@ -18,7 +20,7 @@ export default function Footer() {
                         </div>
                     </div>
                 </div>
-                <p className="w-full text-center text-base text-white/80">&copy; {new Date().getFullYear()} Khaled Ashraf. All rights reserved.</p>
+                <h5 className="w-full text-center text-base text-white/80">&copy; {new Date().getFullYear()} Khaled Ashraf. All rights reserved.</h5>
             </div>
         </footer>
     );
